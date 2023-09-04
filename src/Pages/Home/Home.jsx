553dbf93e1f1e0f0275ce78bf1451a2200702f9f
AnandKumar <anandkumar031199@gmail.com>
2023-09-04 22:02:45 +0530

    make another commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -76,7 +76,6 @@ const Home = () => {
           Add To Do
         </button>
       </div>
-      <h1>Hare krishna</h1>
       {loading && <p style={{ margin: "1rem 0" }}>Loading...</p>}
       <List />
     </div>
